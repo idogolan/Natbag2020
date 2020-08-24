488d4ebbf01bdf180e035774a1aed8fc2526a148
yuvalitach <yuvalitach1@gmail.com>
2020-08-24 20:42:14 +0300

    Python path change @roma321m

--- src/python/Natbag2020WebServerFlask.py
+++ src/python/Natbag2020WebServerFlask.py
@@ -4,28 +4,29 @@ from flask import Flask, request
 
 app = Flask("Natbag2020")
 
-def allFlights(direction):
+
+def allflights(direction):
     return subprocess.check_output(["java", "-classpath",
-                                    "bin", "core.Program",
-                                    request.args.get('outformat'), direction,
+                                    r"C:\Users\יובל\eclipse-workspace\Nattbag2020\Natbag2020\bin", "core.Program",
+                                    request.args.get('outformat'), direction,  # check if departures is args[1]
                                     request.args.get('company'), request.args.get('country'),
                                     request.args.get('day1'), request.args.get('month1'),
                                     request.args.get('year1'), request.args.get('day2'),
                                     request.args.get('month2'), request.args.get('year2'),
                                     request.args.get('sunday'), request.args.get('monday'),
-                                    request.args.get('tuesday'),request.args.get('wednesday'),
+                                    request.args.get('tuesday'), request.args.get('wednesday'),
                                     request.args.get('thursday'), request.args.get('friday'),
                                     request.args.get('saturday')])
-									
+
 
 @app.route("/departures")
 def depart():
-	return allFlights("departures")
-	
+    return allflights("departures")
+
 
 @app.route("/arrivals")
 def arrival():
-	return allFlights("arrivals")
-	
+    return allflights("arrivals")
+
 
 app.run(port=8000, host="0.0.0.0")
